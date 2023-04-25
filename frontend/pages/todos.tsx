ef3e2081a4c279e2da3486a4788f3e7f936dddc0
bluecoolgod80@gmail.com <bluecoolgod80@gmail.com>
2023-04-25 09:22:31 +0900

    [fix] Recoil Todo앱 완성함

--- frontend/pages/todos.tsx
+++ frontend/pages/todos.tsx
@@ -1,19 +1,27 @@
 import React from "react";
-import { useRecoilValue, useSetRecoilState } from "recoil";
+import { useRecoilState, useRecoilValue, useSetRecoilState } from "recoil";
 import { removeTodo, todoListState, toggleTodo } from "../store/atoms";
-import TodoItemCreator from "../components/todoItemCreator";
+import TodoItemCreator from "../components/TodoItemCreator";
 
 function Todos() {
-  //useRecoilValue로 상태를 가져올 수 있다.
-  const todoList = useRecoilValue(todoListState);
-  const setTodoList = useSetRecoilState(todoListState);
+  //useRecoilValue로 상태와 setRecoilValue를 가져올 수 있다.
+
+  // useRecoilState, useRecoilValue, useSetRecoilState의 차이.
+  // const todoList = useRecoilValue(todoListState);
+  // const setTodoList = useSetRecoilState(todoListState);
+  const [todoList, setTodoList] = useRecoilState(todoListState);
+
   return (
     <div>
       <div>todos</div>
       <TodoItemCreator />
+
       {todoList.map((todoItem) => (
         <div key={todoItem.id}>
-          <p onClick={() => toggleTodo(todoItem, setTodoList)}>{todoItem}</p>
+          <p role="button" onClick={() => toggleTodo(todoItem, setTodoList)}>
+            {todoItem.text}
+            {todoItem.completed ? "완료됨" : ""}
+          </p>
           <div role="button" onClick={() => removeTodo(todoItem, setTodoList)}>
             삭제하기
           </div>
