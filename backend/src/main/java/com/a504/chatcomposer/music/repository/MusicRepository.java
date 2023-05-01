f69085ae76215183dbafb4ba9a8107c656b1b43b
박주희 <dam2.dev@gmail.com>
2023-05-01 17:56:28 +0900

    :bug: MusicRepository 수정

--- backend/src/main/java/com/a504/chatcomposer/music/repository/MusicRepository.java
+++ backend/src/main/java/com/a504/chatcomposer/music/repository/MusicRepository.java
@@ -1,7 +1,11 @@
 package com.a504.chatcomposer.music.repository;
 
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import com.a504.chatcomposer.music.entity.Music;
+
 @Repository
-public class MusicRepository {
+public interface MusicRepository extends JpaRepository<Music, Long> {
+
 }
