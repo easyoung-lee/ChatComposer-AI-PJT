701c0b1f6976c53700945ae94fab513b867ad826
박주희 <dam2.dev@gmail.com>
2023-05-03 01:23:06 +0900

    :sparkles: Querydsl 사용을 위한 생성자 추가

--- backend/src/main/java/com/a504/chatcomposer/music/dto/response/MusicsResp.java
+++ backend/src/main/java/com/a504/chatcomposer/music/dto/response/MusicsResp.java
@@ -2,20 +2,25 @@ package com.a504.chatcomposer.music.dto.response;
 
 import java.time.LocalDateTime;
 import java.util.List;
+import java.util.stream.Collectors;
 
 import com.a504.chatcomposer.music.dto.Member;
 import com.a504.chatcomposer.music.dto.enums.Genre;
+import com.a504.chatcomposer.music.entity.Music;
 import com.fasterxml.jackson.annotation.JsonProperty;
+import com.querydsl.core.annotations.QueryProjection;
 
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+import lombok.ToString;
 
 @Getter
 @Setter
 @Builder
+@ToString
 @NoArgsConstructor
 @AllArgsConstructor
 public class MusicsResp {
@@ -43,4 +48,21 @@ public class MusicsResp {
 
 	@JsonProperty(value = "cover_source")
 	private String coverSource;
+
+	@QueryProjection
+	public MusicsResp(Music music, Long memberId, String nickname) {
+		this.musicId = music.getId();
+		this.title = music.getTitle();
+		this.genre = music.getGenre();
+		this.favoriteCount = music.getFavoriteCount();
+		this.createdAt = music.getCreatedAt();
+		this.coverSource = music.getCoverSource();
+		
+		this.member = Member.builder()
+			.memberId(memberId)
+			.nickname(nickname)
+			.build();
+		this.tags = music.getTags().stream()
+			.map(m -> m.getTagName()).collect(Collectors.toList());
+	}
 }
