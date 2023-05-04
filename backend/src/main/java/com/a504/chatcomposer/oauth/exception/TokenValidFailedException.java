30d2f09764a0924bb94ec73c4534c6511871f3db
zdsa43 <zdsa43@gmail.com>
2023-05-04 13:35:51 +0900

    ✨ 구글 소셜 로그인 스켈레톤 코드 추가

---
+++ backend/src/main/java/com/a504/chatcomposer/oauth/exception/TokenValidFailedException.java
@@ -0,0 +1,12 @@
+package com.a504.chatcomposer.oauth.exception;
+
+public class TokenValidFailedException extends RuntimeException {
+
+    public TokenValidFailedException() {
+        super("Failed to generate Token.");
+    }
+
+    private TokenValidFailedException(String message) {
+        super(message);
+    }
+}
