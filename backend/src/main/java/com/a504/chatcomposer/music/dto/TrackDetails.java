97c240121762c4b931367bfe19c69300ae2d7a02
박주희 <dam2.dev@gmail.com>
2023-05-15 22:37:01 +0900

    :recycle: Track DTO 필드 분리
    - pk를 사용하는 곳과 사용하지 않는 곳이 있어서
      DTO 분리 후 상속을 통해 필드 재사용

--- backend/src/main/java/com/a504/chatcomposer/music/dto/TrackDetails.java
+++ backend/src/main/java/com/a504/chatcomposer/music/dto/TrackDetails.java
@@ -5,25 +5,23 @@ import com.fasterxml.jackson.databind.annotation.JsonNaming;
 
 import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.AllArgsConstructor;
-import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+import lombok.ToString;
 
-@Schema(description = "트랙 정보 DTO + 트랙 pk")
+@Schema(description = "트랙 정보 DTO")
 @Getter
 @Setter
+@ToString
 @NoArgsConstructor
 @AllArgsConstructor
 @JsonNaming(value = PropertyNamingStrategies.SnakeCaseStrategy.class)
-public class Track extends TrackDetails {
+public class TrackDetails {
 
-	@Schema(description = "트랙 pk")
-	private Long trackId;
+	@Schema(description = "음악을 재생하기 위한 midi 악보")
+	private String midiDescription;
 
-	@Builder
-	public Track(String midiDescription, String musicalInstrument, Long trackId) {
-		super(midiDescription, musicalInstrument);
-		this.trackId = trackId;
-	}
+	@Schema(description = "악기명")
+	private String musicalInstrument;
 }
