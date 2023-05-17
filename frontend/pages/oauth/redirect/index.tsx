2ad1e5e2bec620180d734a215661b91fd4beba46
최종현 <bluecoolgod@naver.com>
2023-05-17 22:43:19 +0900

    [etc] 로그인 로직 변경, 앨범커버 주소 복원

--- frontend/pages/oauth/redirect/index.tsx
+++ frontend/pages/oauth/redirect/index.tsx
@@ -1,20 +1,25 @@
-import { useSetRecoilState } from "recoil";
+import { useRecoilState, useSetRecoilState } from "recoil";
 import { authTokenState } from "../../../store/atoms";
 import { useRouter } from "next/router";
 import CssSpinner from "../../../components/cssSpinner";
 import { toastAlert } from "../../../utils/toastAlert";
+import { useEffect } from "react";
 
 function Oauth() {
-  const setAuthToken = useSetRecoilState(authTokenState);
+  const [authToken, setAuthToken] = useRecoilState(authTokenState);
   const router = useRouter();
 
   //로컬환경 테스트 주소 http://localhost:3000/oauth/redirect?token=eyJhbGciOiJIUzI1NiJ9.eyJzdWIiOiIxMDgwNDE3NTg0ODczNzA3MzkwNjkiLCJyb2xlIjoiUk9MRV9VU0VSIiwiZXhwIjoxNjg0MjQzNTM4fQ.bSvre-LYNtmeLQVuXXOy56DVCxEqdhoULDAuHiA5uDU
-  const { token } = router.query;
+  const { token } = router.query as { token: string };
   if (token) {
-    setAuthToken(token as string);
-    toastAlert("로그인 되었습니다.");
-    router.push("/");
+    setAuthToken(() => token);
   }
+
+  useEffect(() => {
+    if (!authToken) return;
+    toastAlert("로그인 되었습니다.");
+    setTimeout(() => router.push("/"), 100);
+  }, [authToken]);
 
   return (
     <div>
