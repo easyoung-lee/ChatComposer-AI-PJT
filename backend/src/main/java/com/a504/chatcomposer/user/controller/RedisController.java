b02f2d00ab10a3169b8a5bb44e39dc5a7d1ebd59
zdsa43 <zdsa43@gmail.com>
2023-05-17 10:49:08 +0900

    🔨 테스트 컨트롤러 수정

--- backend/src/main/java/com/a504/chatcomposer/user/controller/RedisController.java
+++ backend/src/main/java/com/a504/chatcomposer/user/controller/RedisController.java
@@ -1,6 +1,7 @@
 package com.a504.chatcomposer.user.controller;
 
-import org.springframework.beans.factory.annotation.Autowired;
+import com.a504.chatcomposer.global.util.RedisUtil;
+import lombok.RequiredArgsConstructor;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.core.ValueOperations;
 import org.springframework.http.HttpStatus;
@@ -11,10 +12,11 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequiredArgsConstructor
 public class RedisController {
 
-    @Autowired
-    private RedisTemplate<String, String> redisTemplate;
+    private final RedisTemplate<String, String> redisTemplate;
+    private final RedisUtil redisUtil;
 
     @PostMapping("/redisTest")
     public ResponseEntity<?> addRedisKey() {
@@ -29,6 +31,14 @@ public class RedisController {
     public ResponseEntity<?> getRedisKey(@PathVariable String key) {
         ValueOperations<String, String> vop = redisTemplate.opsForValue();
         String value = vop.get(key);
+        return new ResponseEntity<>(value, HttpStatus.OK);
+    }
+
+    @GetMapping("/redisTest/limit/{key}")
+    public ResponseEntity<?> getRedisLimitKey(@PathVariable String key) {
+        ValueOperations<String, String> vop = redisTemplate.opsForValue();
+        String value = vop.get(key);
+        redisUtil.setDataExpire(key,key,10000);
         return new ResponseEntity<>(value, HttpStatus.OK);
     }
 
